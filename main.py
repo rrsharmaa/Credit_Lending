c69c34edf7b8836bed2e7a8ce03ee7e6bf6f0b41
rrsharmaa <167867960+rrsharmaa@users.noreply.github.com>
2025-03-14 11:06:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from pyspark.sql import SparkSession
-from src.data_ingestion import read_csv, read_json
-from src.datat_quality_checker import  DataQualityEnhancer
-from src.data_processing import calculate_collateral_value
-from src.data_storage import save_as_parquet
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+
+from etl.reading import load_csv, load_json
+from etl.quality_data_check import QualityDataCheck
+from etl.transformation import evaluate_collateral_value
+from etl.storage import save_as_parquet
 from logger import setup_logger  # Import the setup_logger function
-import warnings
 from constants import LOG_FILE
-# Ignore all warnings for better clarity
-warnings.simplefilter('ignore')
+
 
 def main():
     # Initialize logger
@@ -20,41 +21,41 @@ def main():
     logger.info("Spark session started")
 
     # Paths to data
-    clients_path = "data/input_data/Clients.csv"
-    collaterals_path = "data/input_data/Collaterals.csv"
-    stocks_path = "data/input_data/Stocks.json"
+    clients_path = "data/Clients.csv"
+    collaterals_path = "data/Collaterals.csv"
+    stocks_path = "data/Stocks.json"
     output_path = "data/output_data/collateral_status.parquet"
 
     # Ingest data
     logger.info("Ingesting data")
-    clients_df = read_csv(spark, clients_path)
-    collaterals_df = read_csv(spark, collaterals_path)
-    stocks_df = read_json(spark, stocks_path)
+    clients_df = load_csv(spark, clients_path)
+    collaterals_df = load_csv(spark, collaterals_path)
+    stocks_df = load_json(spark, stocks_path)
 
     #  Data Quality Check
     logger.info("Checking data quality")
-    # enhancer = DataQualityEnhancer(stocks_df)
-    # cleaned_stocks_df = enhancer.trim_extra_spaces(["symbol"]).remove_duplicates(["date", "symbol"]).get_dataframe()
-    # canonicalize_symbol = udf(lambda x: x.upper() if x else None, StringType())
-    # cleaned_stocks_df = enhancer.canonicalize_data("symbol", canonicalize_symbol).get_dataframe()
-    # cleaned_stocks_df.show()
+    enhancer = QualityDataCheck(stocks_df)
+    cleaned_stocks_df = enhancer.trim_columns(["symbol"]).remove_duplicates(["date", "symbol"]).get_dataframe()
+    canonicalize_symbol = udf(lambda x: x.upper() if x else None, StringType())
+    cleaned_stocks_df = enhancer.apply_transformation("symbol", canonicalize_symbol).get_dataframe()
+    cleaned_stocks_df.show()
 
     # Apply similar steps for CSV data
-    # collaterals_df = spark.read.csv("/path/to/Collaterals.csv", header=True)
-    # enhancer = DataQualityEnhancer(collaterals_df)
-    # cleaned_collaterals_df = enhancer.trim_extra_spaces(["Account_ID", "Savings", "Cars", "Stocks"]).remove_duplicates(
-    #     ["Account_ID"]).get_dataframe()
-    #
-    # cleaned_collaterals_df.show()
+    collaterals_df = spark.read.csv("/path/to/Collaterals.csv", header=True)
+    enhancer = QualityDataCheck(collaterals_df)
+    cleaned_collaterals_df = enhancer.trim_columns(["Account_ID", "Savings", "Cars", "Stocks"]).remove_duplicates(
+        ["Account_ID"]).get_dataframe()
+
+    cleaned_collaterals_df.show()
 
     # Additional canonicalization and quality checks can be implemented similarly
 
     # Process data
     logger.info("Calculating collateral value")
-    collateral_status_df = calculate_collateral_value(clients_df, collaterals_df, stocks_df)
+    collateral_status_df = evaluate_collateral_value(clients_df, collaterals_df, stocks_df)
     # df = spark.createDataFrame(collateral_status_df)
 
-    # Save dataggi
+    # Save data
     logger.info(f"Saving results to {output_path}")
     save_as_parquet(collateral_status_df, output_path)
 
